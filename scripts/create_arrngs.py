86ff75bd3b836965eb43f0693482a7cdd0060917
bojikian <bojikian@informatik.hu-berlin.de>
2024-06-07 18:06:12 +0200

    updated graphs

--- scripts/create_arrngs.py
+++ scripts/create_arrngs.py
@@ -1,35 +1,32 @@
 #!/bin/python3
 import os
+from sys import argv
 
 def list_files(directory):
-    file_names = []
-    for root, _, files in os.walk(directory):
-        for file in files:
-            file_names.append(os.path.join(root, file))
-    return file_names
+    return next(folder for folder in os.walk(directory, topdown=True))[2]
 
 # Verzeichnispfad, den Sie durchsuchen möchten
-directory = 'tiny/arrg'
+directory = argv[1]
 files = list_files(directory)
 all_data = []
 for file in files:
-    input_stream = open(file, 'r')
+    file = file[:-len('.gr')]
+    input_file = directory + file + '.gr'
+    output_file = directory + '/arrg/' + file + '_arrg.gr'
+    print(input_file, output_file)
+    input_stream = open(input_file, 'r')
     input = input_stream.read().split('\n')
-    output_file = file[:len(file)-len('.gr')]+'_arrg.gr'
-    print(output_file)
     output = open(output_file, 'w')
     for line in input:
         if len(line) > 0 and line[0] == 'p':
             line = line.split(' ')
             n1 = int(line[2])
             n2 = int(line[3])
-            line.append(str(n1+n2))
+            line.append(str(n1*n2))
             line = ' '.join(line)
             output.write(line+'\n')
             for i in range(n1+n2):
                 output.write(str(i+1)+'\n')
-            continue
-        output.write(line + '\n')
-
-    input_stream.close()
-    os.remove(file)
+        else:
+            if(len(line) > 0):
+                output.write(line + '\n')
